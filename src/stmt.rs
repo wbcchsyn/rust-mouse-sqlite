55457aee237b81a93a5ddd675ca2c85c5ccb30f9
Yoshida Shin <wbcchsyn@gmail.com>
2021-05-30 15:18:13 +0900

    Create method 'Stmt.bind_blob()'

--- src/stmt.rs
+++ src/stmt.rs
@@ -54,10 +54,10 @@
 use crate::Error;
 use core::convert::TryFrom;
 use libsqlite3_sys::{
-    sqlite3_bind_int64, sqlite3_clear_bindings, sqlite3_finalize, sqlite3_reset, sqlite3_step,
-    sqlite3_stmt, SQLITE_RANGE,
+    sqlite3_bind_blob, sqlite3_bind_int64, sqlite3_clear_bindings, sqlite3_destructor_type,
+    sqlite3_finalize, sqlite3_reset, sqlite3_step, sqlite3_stmt, SQLITE_RANGE, SQLITE_TOOBIG,
 };
-use std::os::raw::c_int;
+use std::os::raw::{c_int, c_void};
 
 /// Wrapper of C [`sqlite3_stmt`] .
 ///
@@ -159,6 +159,40 @@ impl Stmt {
 
         let index = c_int::try_from(index).map_err(|_| Error::new(SQLITE_RANGE))?;
         let code = unsafe { sqlite3_bind_int64(self.raw, index, val) };
+        match Error::new(code) {
+            Error::OK => Ok(()),
+            e => Err(e),
+        }
+    }
+
+    /// Wrapper of C function [`sqlite3_bind_blob`] .
+    ///
+    /// Calls method [`reset`] if the privious [`step`] returns `true` , and calls
+    /// [`sqlite3_bind_blob`] .
+    /// (It is necesarry to call [`sqlite3_reset`] after [`sqlite3_step`] , however, [`step`]
+    /// did not call [`sqlite3_reset`] when it returned `true` .)
+    ///
+    /// Note that `index` starts at 1, not 0.
+    ///
+    /// [`reset`]: #method.reset
+    /// [`step`]: #method.step
+    /// [`sqlite3_bind_blob`]: https://www.sqlite.org/c3ref/bind_blob.html
+    /// [`sqlite3_reset`]: https://www.sqlite.org/c3ref/reset.html
+    /// [`sqlite3_step`]: https://www.sqlite.org/c3ref/step.html
+    pub fn bind_blob<'a, 'b>(&'a mut self, index: usize, val: &'b [u8]) -> Result<(), Error>
+    where
+        'b: 'a,
+    {
+        if self.is_row {
+            self.reset();
+        }
+
+        let index = c_int::try_from(index).map_err(|_| Error::new(SQLITE_RANGE))?;
+        let ptr = val.as_ptr() as *const c_void;
+        let len = c_int::try_from(val.len()).map_err(|_| Error::new(SQLITE_TOOBIG))?;
+        const DESTRUCTOR: sqlite3_destructor_type = None;
+
+        let code = unsafe { sqlite3_bind_blob(self.raw, index, ptr, len, DESTRUCTOR) };
         match Error::new(code) {
             Error::OK => Ok(()),
             e => Err(e),
